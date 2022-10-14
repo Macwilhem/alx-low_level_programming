8d0a795e228184ecc84d5903c4622b68440b74d7
Macwilhem <macwilhem@gmail.com>
2022-10-14 16:35:14 -0700

    Variables

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,21 +2,28 @@
 
 /*
  * main-program entry point.
- * Return: 0
+ * Return:  0 no error, non zero if error
  */
 int main(void)
 {
 	char i;
+
 	char j;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		putchar(i);
-	}
-	for (j = 'A'; j <= 'Z'; j++)
-	{
-		putchar(j);
-	}
+	i = 'a';
+	j = 'A';
+	while
+		(i <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(j <= 'Z')
+		{
+			putchar(j);
+			j++;
+		}
 	putchar('\n');
 	return (0);
 }
