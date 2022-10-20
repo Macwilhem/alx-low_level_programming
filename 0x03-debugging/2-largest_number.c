83326c6895e664729a4bea52c544f3a391b60626
Macwilhem <macwilhem@gmail.com>
2022-10-20 05:15:34 -0700

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints the largest of 3 integers
+ * largest_number- returns the largest of 3 integers
  * @a: first integer
  * @b: second integer
  * @c: third integer
