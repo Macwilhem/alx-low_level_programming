d99aa417b4a5b5a39dc36014a715dd8386ceffc5
Macwilhem <macwilhem@gmail.com>
2022-10-24 17:20:43 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,12 +1,9 @@
-#include <unistd.h>
 #include "main.h"
-
-/*
- *_putchar - writes a character c to stdout
- *
- *@c: character to be printed
- *
- *Return: on success 1
+#include <unistd.h>
+/**
+ * _putchar - writes character c to stdout
+ * @c: char to be printed
+ * Return: on success 1
  */
 int _putchar(char c)
 {
